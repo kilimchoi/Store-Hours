ceac041a677d2b80a7142bb499727b3a1c9b3b7d
Ki Lim Choi <kl_choi@berkeley.edu>
2013-01-03 15:31:45 -0800

    adding store_hours

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store_locator.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store_hours.settings")
 
     from django.core.management import execute_from_command_line
 
---
+++ store_hours/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+from store_hours.views import index, store_display, branches_display, address
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    ('^index/$', index),
+    ('^specific_store_display/$', store_display),
+    ('^stores_branches/$', branches_display),
+    ('^address/$', address),
+    # Examples:
+    # url(r'^$', 'store_locator.views.home', name='home'),
+    # url(r'^store_locator/', include('store_locator.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
